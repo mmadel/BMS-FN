29f2f7ec54bc31d49036e2f0020d89cda3e9b097
mmadel <mohammed.adel82@gmail.com>
2023-02-22 13:00:52 +0200

    add dummy data and adjust table with them

--- src/app/modules/patient/list/patient-list.component.ts
+++ src/app/modules/patient/list/patient-list.component.ts
@@ -8,8 +8,7 @@ import usersData from './_data';
 export class PatientListComponent implements OnInit {
 
   constructor() { }
-  roles = [...new Set(usersData.map(item => item.role))];
-  selected: string[] = ['Staff', 'Admin'];
+  
   ngOnInit(): void {
   }
   usersData = usersData;
@@ -18,57 +17,27 @@ export class PatientListComponent implements OnInit {
   columns = [
     {
       key: 'name',
-      _style: { width: '40%' }
+      _style: { width: '30%' }
     },
-    'registered',
-    { key: 'role', _style: { width: '20%' } },
-    { key: 'status', _style: { width: '15%' } },
+    { key: 'birthDate', _style: { width: '10%' } },
+    { key: 'email', _style: { width: '20%' } },
+    { key: 'insurance', _style: { width: '15%' } },
     {
       key: 'show',
       label: '',
       _style: { width: '5%' },
       filter: false,
       sorter: false
+    },
+    {
+      key: 'delete',
+      label: '',
+      _style: { width: '5%' },
+      filter: false,
+      sorter: false
     }
   ];
-  set columnFilterValue(value) {
-    this._columnFilterValue = { ...value };
-    if (!Object.entries(value).length) {
-      this.selected = [];
-    }
-  }
-
-  get columnFilterValue() {
-    return this._columnFilterValue;
-  }
-
-  private _columnFilterValue: any = {};
-  handleValueChange($event: any) {
-    const columnFilterValue = { ...this.columnFilterValue };
-    if ($event?.length) {
-      const selected = [...$event];
-      this.selected = selected;
-      const filterFunction = (item: any) => selected.includes(item);
-      this.columnFilterValue = { ...columnFilterValue, role: filterFunction };
-      return;
-    }
-    delete columnFilterValue.role;
-    this.columnFilterValue = { ...columnFilterValue };
-  }
-  getBadge(status: string) {
-    switch (status) {
-      case 'Active':
-        return 'success';
-      case 'Inactive':
-        return 'secondary';
-      case 'Pending':
-        return 'warning';
-      case 'Banned':
-        return 'danger';
-      default:
-        return 'primary';
-    }
-  }
+ 
 
   details_visible = Object.create({});
 
