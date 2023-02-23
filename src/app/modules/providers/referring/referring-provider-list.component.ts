811fbd7e9cb4f48534f420fabee67ab57aef2d22
mmadel <mohammed.adel82@gmail.com>
2023-02-23 19:45:05 +0200

    impl referring provider list component

--- src/app/modules/providers/referring/referring-provider-list.component.ts
+++ src/app/modules/providers/referring/referring-provider-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import usersData from '../../patient/list/_data';
 @Component({
   selector: 'app-referring-provider-list',
   templateUrl: './referring-provider-list.component.html',
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReferringProviderListComponent implements OnInit {
 
+  usersData = usersData;
+  columns = [
+    {
+      key: 'name',
+      _style: { width: '20%' }
+    },
+    {
+      key: 'id',
+      _style: { width: '5%' }
+    },
+    {
+      key: 'edit',
+      label: '',
+      _style: { width: '1%' },
+      filter: false,
+      sorter: false
+    },
+    {
+      key: 'delete',
+      label: '',
+      _style: { width: '1%' },
+      filter: false,
+      sorter: false
+    }
+  ];
+
+  details_visible = Object.create({});
+
+  toggleDetails(item: any) {
+    this.details_visible[item] = !this.details_visible[item];
+  }
   constructor() { }
 
   ngOnInit(): void {
