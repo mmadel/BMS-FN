42998f43588b3669d9711b135c7a42ae0483748a
mmadel <mohammed.adel82@gmail.com>
2023-02-26 19:48:05 +0200

    add required modules to invoice module

--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IconModule } from '@coreui/icons-angular';
 import { InvoiceRoutingModule } from './invoice-routing.module';
+
+import {
+  AlertModule,
+  BadgeModule,
+  ButtonModule,
+  CardModule,
+  CollapseModule,
+  GridModule,
+  SharedModule,
+  SmartTableModule,
+  TableModule,
+  FormModule,
+  DatePickerModule,
+  DropdownModule,
+  ButtonGroupModule,
+  ListGroupModule,
+  TooltipModule,
+  TabsModule,
+  NavModule,
+  DateRangePickerModule,
+  TimePickerModule,
+  UtilitiesModule,
+  ModalModule
+} from '@coreui/angular-pro';
 
 import {
   InvoicingAreaComponent,
@@ -19,7 +43,29 @@ import {
   ],
   imports: [
     CommonModule,
-    InvoiceRoutingModule
+    InvoiceRoutingModule,
+    AlertModule,
+    BadgeModule,
+    ButtonModule,
+    CardModule,
+    CollapseModule,
+    GridModule,
+    SharedModule,
+    SmartTableModule,
+    TableModule,
+    FormModule,
+    DatePickerModule,
+    DropdownModule,
+    ButtonGroupModule,
+    ListGroupModule,
+    TooltipModule,
+    TabsModule,
+    NavModule,
+    DateRangePickerModule,
+    TimePickerModule,
+    IconModule,
+    UtilitiesModule,
+    ModalModule
   ]
 })
 export class InvoiceModule { }
