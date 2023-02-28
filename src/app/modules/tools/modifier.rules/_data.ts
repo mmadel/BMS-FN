ace212ee6b6768e6ac5709ac185f2041a5242575
mmadel <mohammed.adel82@gmail.com>
2023-02-28 14:14:04 +0200

    impl modifer rules component

---
+++ src/app/modules/tools/modifier.rules/_data.ts
@@ -0,0 +1,7 @@
+const rulesData = [
+    { id: 21, modifier: 'GP', CPT: 'All CPT', addto: 'BEGIN', perm: 'Y' },
+    { id: 21, modifier: 'NU', CPT: 'A4595', addto: 'REPLACE', perm: 'Y' },
+    { id: 21, modifier: 'CR', CPT: 'G2061', addto: 'REPLACE', perm: 'Y' },
+    { id: 21, modifier: 'GP.59', CPT: '97164', addto: 'REPLACE', perm: 'Y' },
+]
+export default rulesData
--- src/app/modules/tools/modifier.rules/modifier-rule.component.ts
+++ src/app/modules/tools/modifier.rules/modifier-rule.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import usersData from './_data';
 @Component({
   selector: 'app-modifier-rule',
   templateUrl: './modifier-rule.component.html',
   styleUrls: ['./modifier-rule.component.scss']
 })
 export class ModifierRuleComponent implements OnInit {
-
+  usersData = usersData
+  columns = [
+    'modifier',
+    'CPT',
+    'addto',
+    'perm',
+    {
+      key: 'delete',
+      label: '',
+      _style: { width: '20%' }
+    }
+  ];
+  clickAddRule(){
+    
+  }
   constructor() { }
 
   ngOnInit(): void {
