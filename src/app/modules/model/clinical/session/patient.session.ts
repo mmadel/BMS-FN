299f10d81f9d63c052d3607c71bb2070d3ac7cf5
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-11-28 12:02:41 +0200

    add clinic info to session model

--- src/app/modules/model/clinical/session/patient.session.ts
+++ src/app/modules/model/clinical/session/patient.session.ts
@@ -1,4 +1,5 @@
 import { CaseDiagnosis } from "../case.diagnosis"
+import { ClinicInfo } from "./clinic.info";
 import { DoctorInfo } from "./doctor.info";
 import { PateintInfo } from "./patient.info";
 import { ServiceLine } from "./service.line"
@@ -7,6 +8,7 @@ export interface PatientSession {
     id?: number
     patientInfo: PateintInfo;
     doctorInfo?: DoctorInfo;
+    ClinicInfo?: ClinicInfo
     serviceDate?: number;
     serviceStartTime?: number
     serviceEndTime?: number
