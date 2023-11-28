1ec69eaf96a5c34ea625a6934e8b71eb60e08c25
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-11-28 21:42:11 +0200

    create service to create and find all providers

---
+++ src/app/modules/model/clinical/provider/provider.info.ts
@@ -0,0 +1,9 @@
+export interface ProviderInfo{
+    professionAbbr?:string
+    pqrs?:string
+    taxonomyCode?:string
+    credentials?:string
+    group?:string
+    taxId?:string
+    license?:string
+}
