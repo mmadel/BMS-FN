4ba05b417bb2fdc077609ef6a7cce440778c2116
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-06 23:13:18 +0200

    list clinic components and services

--- src/app/modules/admin.tools/components/clinics/clinics.component.ts
+++ src/app/modules/admin.tools/components/clinics/clinics.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+import { Clinic } from 'src/app/modules/model/admin/clinic';
+import { ListTemplate } from 'src/app/modules/model/template/list.template';
+import { ClinicService } from '../../services/clinic.service';
 
 @Component({
   selector: 'app-clinics',
   templateUrl: './clinics.component.html',
   styleUrls: ['./clinics.component.scss']
 })
-export class ClinicsComponent implements OnInit {
-
-  constructor() { }
+export class ClinicsComponent extends ListTemplate implements OnInit {
+  clinics$!: Observable<Clinic[]>;
+  columns = [
+    {
+      key: 'title',
+      label: 'Clinic Title',
+      _style: { width: '10%' }
+    },
+    { key: 'clinicdata', label: 'Address', _style: { width: '10%' } },
+    { key: 'actions', _style: { width: '5%' } }
+  ];
+  constructor(private clinicService: ClinicService) { super() }
 
   ngOnInit(): void {
+    this.initListComponent();
+    this.find();
   }
 
+  public find() {
+    this.clinics$ = this.clinicService.findAll(this.apiParams$).pipe(
+      tap((response: any) => {
+        this.totalItems$.next(response.number_of_matching_records);
+        if (response.number_of_records) {
+          this.errorMessage$.next('');
+        }
+        this.retry$.next(false);
+        this.loadingData$.next(false);
+      }),
+      map((response: any) => {
+        return response.records;
+      })
+    )
+  }
+  remove(item: any) {
+
+  }
+  edit(item: any) {
+
+  }
 }
--- src/app/modules/admin.tools/services/clinic.service.ts
+++ src/app/modules/admin.tools/services/clinic.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Clinic } from '../../model/admin/clinic';
+import { IApiParams } from '../../model/interface/api.params';
 import { BasePaginationService } from '../../model/service/base-pagination.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClinicService extends BasePaginationService {
-  private baseUrl = environment.baseURL + 'organization'
+  private baseUrl = environment.baseURL + 'clinic'
 
   constructor(httpClient: HttpClient) { super(httpClient) }
 
@@ -22,5 +24,9 @@ export class ClinicService extends BasePaginationService {
     const headers = { 'content-type': 'application/json' }
     var url = this.baseUrl + '/update'
     return this.httpClient.put(`${url}`, JSON.stringify(clinic), { 'headers': headers })
+  }
+  public findAll(config$: BehaviorSubject<IApiParams>): Observable<any> {
+    var url = this.baseUrl + '/find'
+    return this.get(config$, url)
   }
 }
