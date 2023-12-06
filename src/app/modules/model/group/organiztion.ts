d32c722b8e5b9b1e1636edb769a2c0c24b25d6c0
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-06 20:23:33 +0200

    create organziation service and model

---
+++ src/app/modules/model/group/organiztion.ts
@@ -0,0 +1,16 @@
+export interface Organization {
+    busniessname?: string,
+    firstname?: string,
+    lastname?: string,
+    npi?: string,
+    taxid?: string,
+    taxonomy?: string,
+    address?: string,
+    addresstwo?: string,
+    city?: string,
+    state?: string,
+    zipcode?: string,
+    phone?: string,
+    fax?: string,
+    email?: string
+}
