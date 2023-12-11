6dafbf186cfec6e9f4d079207037cb1c545c84e9
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-11 12:33:47 +0200

    create insurance company configuration model

---
+++ src/app/modules/model/admin/insurance.company.configuration.ts
@@ -0,0 +1,9 @@
+import { Organization } from "./organiztion";
+
+export interface InsuranceCompanyConfiguration{
+    box32?:boolean;
+    box26:string;
+    box33?:boolean;
+    billingProvider?:Organization
+
+}
