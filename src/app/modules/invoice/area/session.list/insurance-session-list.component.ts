41fe8b4e2ca977cffd0b8712e9572ff61b4ead45
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-13 15:44:16 +0200

    switch place , cpt ,unit and charge as links in session list

--- src/app/modules/invoice/area/session.list/insurance-session-list.component.ts
+++ src/app/modules/invoice/area/session.list/insurance-session-list.component.ts
@@ -35,10 +35,22 @@ export class InsuranceSessionListComponent implements OnInit, AfterViewInit {
     'dos',
     'provider',
     'case',
-    'place',
-    'cpt',
-    'unit',
-    'charge',
+    {
+      key:'place',
+      _style: { width: '5%' },
+    },
+    {
+      key:'cpt',
+      _style: { width: '8%' },
+    },
+    {
+      key:'unit',
+      _style: { width: '5%' },
+    },
+    {
+      key:'charge',
+      _style: { width: '8%' },
+    },
     {
       key: 'actions',
       label: '',
