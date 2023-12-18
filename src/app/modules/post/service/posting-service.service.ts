42548bc00293b06469f60cac865d8ab6004f645a
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-18 16:57:46 +0200

    impl create  Client Payments service

--- src/app/modules/post/service/posting-service.service.ts
+++ src/app/modules/post/service/posting-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IApiParams } from '../../model/interface/api.params';
+import { PaymentServiceLine } from '../../model/posting/payment.service.line';
 import { BasePaginationService } from '../../model/service/base-pagination.service';
 
 @Injectable({
@@ -15,5 +16,11 @@ export class PostingServiceService extends BasePaginationService  {
   public findClientPayments(clientId:number): Observable<any> {
     var url = this.baseUrl + '/find/client/'+clientId
     return this.httpClient.get(url)
+  }
+
+  public createClientPayments( paymentServiceLine:PaymentServiceLine,clientId:number) {
+    const headers = { 'content-type': 'application/json' }
+    var url = this.baseUrl + '/create/payments/clientId/' + clientId
+    return this.httpClient.post(`${url}`, JSON.stringify(paymentServiceLine), { 'headers': headers })
   }
 }
