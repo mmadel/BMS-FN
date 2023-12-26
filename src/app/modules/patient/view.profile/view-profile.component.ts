80047dd8af2099b328eb9248cf401439095677bc
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-26 17:39:16 +0200

    show basic and address information in the modal

--- src/app/modules/patient/view.profile/view-profile.component.ts
+++ src/app/modules/patient/view.profile/view-profile.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { Patient } from '../../model/clinical/patient';
+import { Country } from '../../model/common/country';
+import { Gender } from '../../model/enum/geneder';
+import { MaritalStatus } from '../../model/enum/marital.status';
+import { PhoneType } from '../../model/enum/phone.type';
+import { Countries } from '../../model/lookups/country-data-store';
+import { States } from '../../model/lookups/state-data-store';
 
 @Component({
   selector: 'view-patient-profile',
@@ -7,10 +14,20 @@ import { Patient } from '../../model/clinical/patient';
   styleUrls: ['./view-profile.component.scss']
 })
 export class ViewProfileComponent implements OnInit {
-  @Input() selectedPatient:Patient
+  @Input() patient:Patient
+  genders = Gender;
+  genderKeys = Object.values;
+  maritalStatuses = MaritalStatus;
+  maritalStatusKeys = Object.values;
+  phoneTypes = PhoneType
+  phoneTypesKeys = Object.values;
+  countries: Country[] = Countries;
+  states: string[] = States;
+  patientDOB: Date
   constructor() { }
 
   ngOnInit(): void {
+    this.patientDOB = moment.unix(this.patient.birthDate / 1000).toDate();
   }
 
 }
