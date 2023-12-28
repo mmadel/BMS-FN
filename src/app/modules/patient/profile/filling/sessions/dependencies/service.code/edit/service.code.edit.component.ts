3478339419616c0208b045014835d71bed2bcb1b
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-28 16:59:05 +0200

    show and edit daagnosis in edit service code

--- src/app/modules/patient/profile/filling/sessions/dependencies/service.code/edit/service.code.edit.component.ts
+++ src/app/modules/patient/profile/filling/sessions/dependencies/service.code/edit/service.code.edit.component.ts
@@ -9,22 +9,32 @@ import { EmitPatientSessionService } from 'src/app/modules/patient/service/sessi
   styleUrls: ['./service.code.edit.component.scss']
 })
 export class ServiceCodeEditComponent implements OnInit {
-  modifier:string[]=[]
+  modifier: string[] = []
+  diagnosisCodes: string[];
+  selectedDiagnosisCodes: string[];
   constructor(private emitPatientSessionService: EmitPatientSessionService) { }
   selectedServiceCode: ServiceCode;
   @Output() changeVisibility = new EventEmitter<string>()
   ngOnInit(): void {
+    this.emitPatientSessionService.diagnosisCodes$.pipe(
+      filter(result => result !== null)
+    ).subscribe((result) => {
+      this.diagnosisCodes = result;
+    })
+
     this.emitPatientSessionService.sessionserviceCode$.pipe(
       filter((selectedServiceCode) => selectedServiceCode !== null),
       first()
     ).subscribe((selectedServiceCode) => {
+      this.selectedDiagnosisCodes = selectedServiceCode.diagnoses;
       this.modifier = selectedServiceCode.cptCode.modifier.split('.')
       this.selectedServiceCode = selectedServiceCode;
     })
   }
   updateServiceCode() {
-    this.selectedServiceCode.cptCode.modifier= this.modifier.join(".") 
+    this.selectedServiceCode.cptCode.modifier = this.modifier.join(".")
+    this.selectedServiceCode.diagnoses= this.selectedDiagnosisCodes
     this.changeVisibility.emit('close');
   }
-  
+
 }
