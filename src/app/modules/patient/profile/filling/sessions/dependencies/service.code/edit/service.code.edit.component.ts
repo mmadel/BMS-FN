30d80d15c2da4b2d8a2885e65a2ec0f76613fa3c
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-01-02 19:37:54 +0200

    add validation when create or edit service line code

--- src/app/modules/patient/profile/filling/sessions/dependencies/service.code/edit/service.code.edit.component.ts
+++ src/app/modules/patient/profile/filling/sessions/dependencies/service.code/edit/service.code.edit.component.ts
@@ -14,6 +14,10 @@ export class ServiceCodeEditComponent implements OnInit {
   selectedDiagnosisCodes: string[];
   constructor(private emitPatientSessionService: EmitPatientSessionService) { }
   selectedServiceCode: ServiceCode;
+  validModifiers: Boolean[];
+  validCPT: any = ''
+  emptyUnit: boolean = true;
+  emptyCharge: boolean = true;
   @Output() changeVisibility = new EventEmitter<string>()
   ngOnInit(): void {
     this.emitPatientSessionService.diagnosisCodes$.pipe(
@@ -32,9 +36,49 @@ export class ServiceCodeEditComponent implements OnInit {
     })
   }
   updateServiceCode() {
-    this.selectedServiceCode.cptCode.modifier = this.modifier.join(".")
-    this.selectedServiceCode.diagnoses= this.selectedDiagnosisCodes
-    this.changeVisibility.emit('close');
+    var validMod = this.validatModifier();
+    var validCPt = this.validateCPT();
+    var validUniy = this.validateUnit();
+    var validCharge = this.validateCharge();
+    if (validMod.length === 0 && validCPt === -1 && validUniy && validCharge) {
+      this.selectedServiceCode.cptCode.modifier = this.modifier.join(".")
+      this.selectedServiceCode.diagnoses = this.selectedDiagnosisCodes
+      this.changeVisibility.emit('close');
+    }
+  }
+  private validatModifier() {
+    this.validModifiers = new Array();
+    for (let i = 0; i < this.modifier.length; i++) {
+      var mod: string = this.modifier[i];
+      if (mod !== undefined && (mod.length > 0 && mod.length < 2))
+        this.validModifiers[i] = false
+    }
+    return this.validModifiers;
+  }
+
+  private validateCPT() {
+    if (this.selectedServiceCode.cptCode.serviceCode === undefined)
+      this.validCPT = 0
+
+    else if (this.selectedServiceCode.cptCode.serviceCode?.length < 5)
+      this.validCPT = 5
+    else
+      this.validCPT = -1
+    return this.validCPT;
+  }
+  private validateUnit() {
+    if (this.selectedServiceCode.cptCode.unit === undefined)
+      this.emptyUnit = false
+    else
+      this.emptyUnit = true
+    return this.emptyUnit;
+  }
+  private validateCharge() {
+    if (this.selectedServiceCode.cptCode.charge === undefined)
+      this.emptyCharge = false
+    else
+      this.emptyCharge = true;
+    return this.emptyCharge;
   }
 
 }
