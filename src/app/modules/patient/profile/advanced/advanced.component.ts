6950db84e17127cb8d7f402054c31823f551d36a
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-01-18 18:22:47 +0200

    check if undefinded

--- src/app/modules/patient/profile/advanced/advanced.component.ts
+++ src/app/modules/patient/profile/advanced/advanced.component.ts
@@ -25,7 +25,8 @@ export class AdvancedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    if (this.pateint !== undefined && this.pateint.patientAdvancedInformation !== null) {
+    if ((this.pateint !== undefined && this.pateint.patientAdvancedInformation !== null)
+    && (this.pateint.patientAdvancedInformation !== undefined)) {
       this.patientAdvancedInformation = this.pateint.patientAdvancedInformation
       if (this.patientAdvancedInformation.unableToWorkStartDate !== null)
         this.patientAdvancedInformation.unableToWorkStartDate_date = moment.unix(this.patientAdvancedInformation.unableToWorkStartDate / 1000).toDate()
