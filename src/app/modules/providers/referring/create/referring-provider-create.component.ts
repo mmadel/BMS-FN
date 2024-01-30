73fb5efcb0d2a0bbd2c936ca93686c9d37ca68be
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-01-30 18:31:22 +0200

    change referring provider form to accept npi and complete referring provider data

--- src/app/modules/providers/referring/create/referring-provider-create.component.ts
+++ src/app/modules/providers/referring/create/referring-provider-create.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { debounceTime, filter, finalize, switchMap, tap } from 'rxjs';
 import { ReferringProvider } from 'src/app/modules/model/clinical/referring.provider';
+import { ProviderService } from '../../service/provider.service';
 import { ReferringProviderService } from '../../service/referring-provider.service';
 
 @Component({
@@ -10,11 +13,59 @@ import { ReferringProviderService } from '../../service/referring-provider.servi
 })
 export class ReferringProviderCreateComponent implements OnInit {
   @Output() changeVisibility = new EventEmitter<string>()
-  referringProvider: ReferringProvider = {}
+  npiNotFound: boolean = false
+  referringProvider:ReferringProvider;
+  npiReferringProviderCtrl = new FormControl();
+  isLoading = false;
   constructor(private referringProviderService: ReferringProviderService
+    , private providerService: ProviderService
     , private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initModel();
+    this.npiReferringProviderCtrl.valueChanges
+    .pipe(
+      filter(text => {
+        if (!Number(text)) {
+          return false;
+        }
+        if (text === undefined) {
+          return false;
+        }
+        if (text.length > 1) {
+          return true
+        } else {
+          this.initModel();
+          return false;
+        }
+      }),
+      debounceTime(500),
+      tap((value) => {
+        this.initModel();
+        this.isLoading = true;
+      }),
+      switchMap((value) => {
+        return this.providerService.findProviderByNPI(value)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+          )
+      }
+      )
+    ).subscribe(data => {
+      this.npiNotFound = false;
+      this.referringProvider = data;
+      if (this.referringProvider.npi === null) {
+        this.initModel();
+        this.npiNotFound = true;
+      }
+    },
+      error => {
+        this.isLoading = false
+        this.initModel();
+        this.npiNotFound = true;
+      });
   }
   create() {
     this.referringProviderService.create(this.referringProvider)
@@ -23,9 +74,13 @@ export class ReferringProviderCreateComponent implements OnInit {
       }, (error) => {
         this.toastr.error("Error in Referring Provider Creation")
       })
-      this.changeVisibility.emit('close');
+    this.changeVisibility.emit('close');
   }
   resetError() {
-
+  }
+  private initModel() {
+    this.referringProvider = {
+      npi:null
+    }
   }
 }
