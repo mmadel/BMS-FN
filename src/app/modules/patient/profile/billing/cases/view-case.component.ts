00fd62c7b445a793a538e271d98594d5e1ffcfe6
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-02-20 15:19:50 +0200

    switch case whne create or update case to push on list in case of create and hide add and edit modal in both cases

--- src/app/modules/patient/profile/billing/cases/view-case.component.ts
+++ src/app/modules/patient/profile/billing/cases/view-case.component.ts
@@ -16,6 +16,7 @@ export class ViewCaseComponent implements OnInit {
   @ViewChild('caseAddDaignosisComponent') caseAddDaignosisComponent: CaseAddDaignosisComponent;
   addCaseVisibility: boolean = false
   editCaseVisibility: boolean = false;
+  mode: string;
   _cases: PatientCase[]
   editCase: PatientCase;
   constructor(private emitPatientSessionService: EmitPatientSessionService
@@ -43,7 +44,12 @@ export class ViewCaseComponent implements OnInit {
       })
 
   }
+  add() {
+    this.mode = 'create';
+    this.addCaseVisibility = !this.addCaseVisibility
+  }
   edit(selectedCase: PatientCase) {
+    this.mode = 'edit'
     this.editCase = selectedCase
     this.editCaseVisibility = true
   }
@@ -63,9 +69,17 @@ export class ViewCaseComponent implements OnInit {
   createCase() {
     this.patientService.createPatientCase(this.caseAddDaignosisComponent.case, this.patient.id)
       .subscribe((reuslt) => {
-        this._cases.push(this.caseAddDaignosisComponent.case)
-        this.addCaseVisibility = false
-        this.toastr.success("successfully patient case created.")
+        switch (this.mode) {
+          case 'create':
+            this._cases.push(this.caseAddDaignosisComponent.case)
+            this.addCaseVisibility = false
+            this.toastr.success("successfully patient case created.")
+            break;
+          case 'edit':
+            this.editCaseVisibility = false
+            this.toastr.success("successfully patient case updated.")
+        }
+
         this.scrollUp();
       }, error => {
         this.toastr.error("error during create patient case.")
