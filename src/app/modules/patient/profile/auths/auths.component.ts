d520a0652c71c47712beb0368e53e861a58bc832
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-07 10:42:06 +0200

    add create auth modal

--- src/app/modules/patient/profile/auths/auths.component.ts
+++ src/app/modules/patient/profile/auths/auths.component.ts
@@ -10,10 +10,17 @@ export class AuthsComponent implements OnInit {
   public calendarDate = Date.now();
   public startDate?: Date | null = new Date(new Date().setDate(this.date.getDate() + 1));
   public endDate?: Date | null = new Date(new Date().setDate(this.date.getDate() + 3));
+  createAutVisibility:boolean= false;
   constructor() { }
 
   ngOnInit(): void {
     this.calendarDate = Date.now();
+  }
+  toggleCreateAuthVisibility(){
+    this.createAutVisibility =!this.createAutVisibility;
+  }
+  creatAuth(){
+    this.createAutVisibility = true;
   }
 
 }
