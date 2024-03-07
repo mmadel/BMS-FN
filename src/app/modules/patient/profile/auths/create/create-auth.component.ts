55dda54cbf2d128176a94b50d5b1cee1e0f442c4
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-07 10:46:11 +0200

    refer to create auth from auth tab

--- src/app/modules/patient/profile/auths/create/create-auth.component.ts
+++ src/app/modules/patient/profile/auths/create/create-auth.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-create-auth',
+  selector: 'create-auth',
   templateUrl: './create-auth.component.html',
   styleUrls: ['./create-auth.component.scss']
 })
