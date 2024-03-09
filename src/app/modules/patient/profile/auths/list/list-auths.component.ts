0674bbbd6ba68fb5d49d96d1c2c3438ee2107f74
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-09 16:48:43 +0200

    pass patient id as input to list auth component

--- src/app/modules/patient/profile/auths/list/list-auths.component.ts
+++ src/app/modules/patient/profile/auths/list/list-auths.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'list-auths',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-auths.component.scss']
 })
 export class ListAuthsComponent implements OnInit {
-
+  @Input() patientId: number
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.patientId)
   }
 
 }
