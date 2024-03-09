c6d68dd04b0c5df7071979f8a68bfe1d8093b17c
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-09 16:50:22 +0200

    call auth service from list auth component to find auths by patient id

--- src/app/modules/patient/profile/auths/list/list-auths.component.ts
+++ src/app/modules/patient/profile/auths/list/list-auths.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from '../../../service/auth/auth.service';
 
 @Component({
   selector: 'list-auths',
@@ -7,10 +8,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ListAuthsComponent implements OnInit {
   @Input() patientId: number
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
-    console.log(this.patientId)
+    this.authService.find(this.patientId).subscribe((result)=>{
+      console.log(JSON.stringify(result))
+    })
   }
 
 }
