5ce3a258b485733c0ae02165f961950a955944e2
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-09 17:18:16 +0200

    render auths  in tabel with select for each row , catch the selected row to be assigned

--- src/app/modules/patient/profile/auths/list/list-auths.component.ts
+++ src/app/modules/patient/profile/auths/list/list-auths.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PatientAuthorization } from 'src/app/modules/model/clinical/auth/patient.auth';
 import { AuthService } from '../../../service/auth/auth.service';
 
 @Component({
@@ -8,12 +9,19 @@ import { AuthService } from '../../../service/auth/auth.service';
 })
 export class ListAuthsComponent implements OnInit {
   @Input() patientId: number
+  patientAuthorization:PatientAuthorization[]
+  selectedPatientAuthorization: PatientAuthorization;
   constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
-    this.authService.find(this.patientId).subscribe((result)=>{
-      console.log(JSON.stringify(result))
+    this.authService.find(this.patientId).subscribe((result:any)=>{
+      this.patientAuthorization = result;
     })
   }
+  selectAuthorization(){
 
+  }
+  checkedAuth(event:any){
+    this.selectedPatientAuthorization = event;
+  }
 }
