e361d3389bce29a0aae11881962be2a81fab8b0f
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-20 19:16:21 +0200

    add session history model and history table

--- src/app/modules/cms.filing/component/list/find-history.component.ts
+++ src/app/modules/cms.filing/component/list/find-history.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ListTemplate } from 'src/app/modules/model/template/list.template';
 
 @Component({
   selector: 'app-find-history',
   templateUrl: './find-history.component.html',
   styleUrls: ['./find-history.component.scss']
 })
-export class FindHistoryComponent implements OnInit {
+export class FindHistoryComponent extends ListTemplate implements OnInit {
 
-  constructor() { }
+  constructor() { super()}
 
   ngOnInit(): void {
   }
---
+++ src/app/modules/cms.filing/model/session.history.ts
@@ -0,0 +1,8 @@
+export interface SessionHistory {
+    submissionId?: number
+    insuranceCompany?: string
+    submitDate?: Date
+    clinet?: string
+    provider?: string
+    dateOfService: Date
+}
