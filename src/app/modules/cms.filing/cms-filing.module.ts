b450811388c5d091872f9dab966624427dde7592
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-20 16:14:27 +0200

    create cms filing module

---
+++ src/app/modules/cms.filing/cms-filing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CmsFilingRoutingModule } from './cms-filing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CmsFilingRoutingModule
+  ]
+})
+export class CmsFilingModule { }
