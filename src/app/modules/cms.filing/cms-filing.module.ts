5896ed5abe749951c862fc3aac9cafc16785f5cf
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-20 17:00:21 +0200

    add search panel in sessions history

--- src/app/modules/cms.filing/cms-filing.module.ts
+++ src/app/modules/cms.filing/cms-filing.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CmsFilingRoutingModule } from './cms-filing-routing.module';
 import { FindHistoryComponent } from './component/list/find-history.component';
+import { AlertModule, BadgeModule, ButtonModule, CardModule, CollapseModule, GridModule, SharedModule, SmartTableModule, TableModule, FormModule, DatePickerModule, DropdownModule, ButtonGroupModule, ListGroupModule, TooltipModule, TabsModule, NavModule, DateRangePickerModule, TimePickerModule, ModalModule, MultiSelectModule, SpinnerModule, SmartPaginationModule } from '@coreui/angular-pro';
+import { IconModule } from '@coreui/icons-angular';
+import { AutocompleteLibModule } from 'angular-ng-autocomplete';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ModelModule } from '../model/model.module';
 
 
 @NgModule({
@@ -11,7 +16,35 @@ import { FindHistoryComponent } from './component/list/find-history.component';
   ],
   imports: [
     CommonModule,
-    CmsFilingRoutingModule
+    CmsFilingRoutingModule,
+    ModelModule,
+    AlertModule,
+    BadgeModule,
+    ButtonModule,
+    CardModule,
+    CollapseModule,
+    GridModule,
+    SharedModule,
+    SmartTableModule,
+    TableModule,
+    FormModule,
+    DatePickerModule,
+    DropdownModule,
+    ButtonGroupModule,
+    ListGroupModule,
+    IconModule,
+    TooltipModule,
+    TabsModule,
+    NavModule,
+    DateRangePickerModule,
+    TimePickerModule,
+    AutocompleteLibModule,
+    ModalModule,
+    ModelModule,
+    MultiSelectModule,
+    SpinnerModule,
+    SmartPaginationModule,
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
   ]
 })
 export class CmsFilingModule { }
