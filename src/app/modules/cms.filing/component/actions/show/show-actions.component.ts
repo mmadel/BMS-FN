c12f9e5fdc862cac61c44ad629faad0a3dd59ac7
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-22 16:05:01 +0200

    create show action component

---
+++ src/app/modules/cms.filing/component/actions/show/show-actions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'show-actions',
+  templateUrl: './show-actions.component.html',
+  styleUrls: ['./show-actions.component.scss']
+})
+export class ShowActionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
