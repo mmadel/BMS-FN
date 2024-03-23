0d2f4b15269e2fceffd7a083c870ecade884ab38
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-23 23:56:00 +0200

    create stepper in correct claim action modal

--- src/app/modules/cms.filing/cms-filing.module.ts
+++ src/app/modules/cms.filing/cms-filing.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CmsFilingRoutingModule } from './cms-filing-routing.module';
 import { FindHistoryComponent } from './component/list/find-history.component';
-import { CalloutModule,AlertModule, BadgeModule, ButtonModule, CardModule, CollapseModule, GridModule, SharedModule, SmartTableModule, TableModule, FormModule, DatePickerModule, DropdownModule, ButtonGroupModule, ListGroupModule, TooltipModule, TabsModule, NavModule, DateRangePickerModule, TimePickerModule, ModalModule, MultiSelectModule, SpinnerModule, SmartPaginationModule } from '@coreui/angular-pro';
+import { ProgressModule, CalloutModule, AlertModule, BadgeModule, ButtonModule, CardModule, CollapseModule, GridModule, SharedModule, SmartTableModule, TableModule, FormModule, DatePickerModule, DropdownModule, ButtonGroupModule, ListGroupModule, TooltipModule, TabsModule, NavModule, DateRangePickerModule, TimePickerModule, ModalModule, MultiSelectModule, SpinnerModule, SmartPaginationModule } from '@coreui/angular-pro';
 import { IconModule } from '@coreui/icons-angular';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ModelModule } from '../model/model.module';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { SessionHistoryItemComponent } from './component/item/session-history-item.component';
 import { ShowActionsComponent } from './component/actions/show/show-actions.component';
 import { CorrectClaimActionComponent } from './component/actions/correct.claim/correct-claim-action.component';
@@ -52,6 +52,7 @@ import { CorrectClaimActionComponent } from './component/actions/correct.claim/c
     SmartPaginationModule,
     CalloutModule,
     MatPaginatorModule,
+    ProgressModule,
     NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
   ]
 })
--- src/app/modules/cms.filing/component/actions/correct.claim/correct-claim-action.component.ts
+++ src/app/modules/cms.filing/component/actions/correct.claim/correct-claim-action.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./correct-claim-action.component.scss']
 })
 export class CorrectClaimActionComponent implements OnInit {
-
+  cards: { id: number, name: string }[] = [
+    { "id": 1, "name": "Session Selection" },
+    { "id": 2, "name": "Confirmation" },
+  ];
+  counter: number;
+  progressValue: number;
   constructor() { }
 
   ngOnInit(): void {
+    this.counter = 1;
+    this.progressValue = 0;
   }
-
+  next(patientModel: string) {
+    this.proceedToNextStep();
+  }
+  back(patientModel?: string) {
+    this.counter--;
+    this.calculatePercentage(this.counter, 'back');
+  }
+  proceedToNextStep() {
+    this.calculatePercentage(this.counter, 'next');
+    this.counter++;
+  }
+  calculatePercentage(index: number, action: string) {
+    if (action === 'back')
+      index--;
+    this.progressValue = Math.round(((index / this.cards.length) / 100) * 10000);
+  }
+  
 }
