d2c13930300b72d801e26e22f12d2b9e84a38538
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-25 22:39:38 +0200

    add sessionHistoryCriteria interface

---
+++ src/app/modules/cms.filing/model/session.history.criteria.ts
@@ -0,0 +1,10 @@
+export interface sessionHistoryCriteria{
+    insuranceCompany?:string,
+    client?:string,
+    provider?:string,
+    claimId?:string,
+    dosStart?:number,
+    dosEnd?:number,
+    submitStart?:number
+    submitEnd?:number
+}
