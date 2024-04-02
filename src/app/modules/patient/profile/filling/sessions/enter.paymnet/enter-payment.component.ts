d488999c9cafed43db56a21c391ed15de9173a27
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-02 02:43:43 +0200

    get pmt and adj for each service line and show it

--- src/app/modules/patient/profile/filling/sessions/enter.paymnet/enter-payment.component.ts
+++ src/app/modules/patient/profile/filling/sessions/enter.paymnet/enter-payment.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { PatientSession } from 'src/app/modules/model/clinical/session/patient.session';
+import { EnterPaymentService } from 'src/app/modules/patient/service/session/payment/enter-payment.service';
 
 @Component({
   selector: 'enter-payment',
@@ -12,28 +13,48 @@ export class EnterPaymentComponent implements OnInit {
   DOS: string;
   client: string;
   provider: string;
-  totalUnits:number;
-  totalCharge:number
-  constructor() { }
+  totalUnits: number;
+  totalCharge: number
+  totalPmt: number;
+  totalAdj: number
+  totalBalance: number
+  constructor(private enterPaymentService: EnterPaymentService) { }
 
   ngOnInit(): void {
-    this.populateDate();
-    this.calculateNumbers();
+    this.fetchPayment();
   }
   private populateDate() {
     this.DOS = moment.unix(this.session.data.serviceDate / 1000).format('MM/DD/YYYY')
     this.client = this.session.data.patientName;
     this.provider = this.session.data.doctorInfo.doctorLastName + ',' + this.session.data.doctorInfo.doctorFirstName
   }
-  private calculateNumbers() {
+  private calculateNumbers(paymnet: any) {
     this.totalUnits = 0;
     this.totalCharge = 0;
-    let totalPmt = 0;
-    let totalAdj = 0;
-    this.session.serviceCodes
+    this.totalPmt = 0;
+    this.totalAdj = 0;
+    this.totalBalance = 0
+
     for (const obj of this.session.data.serviceCodes) {
+      var _rslt = paymnet.find((pmnts: any) => pmnts.serviceLineId === obj.id);
       this.totalUnits += obj.cptCode.unit;
       this.totalCharge += obj.cptCode.charge;
+      if (_rslt !== undefined) {
+        console.log(JSON.stringify(_rslt))
+        this.totalPmt += _rslt.payment
+        this.totalAdj += _rslt.adjust;
+        this.totalBalance += _rslt.balance
+      }
+    }
   }
+  private fetchPayment() {
+    var serviceLinesIds: number[] = new Array()
+    for (const obj of this.session.data.serviceCodes) {
+      serviceLinesIds.push(obj.id)
+    }
+    this.enterPaymentService.find(serviceLinesIds).subscribe((result: any) => {
+      this.populateDate();
+      this.calculateNumbers(result);
+    })
   }
 }
--- src/app/modules/patient/service/session/payment/enter-payment.service.ts
+++ src/app/modules/patient/service/session/payment/enter-payment.service.ts
@@ -11,6 +11,6 @@ export class EnterPaymentService {
 
   public find(serviceLinesIds:number[]){
     var url = this.baseUrl + '/find/' + serviceLinesIds;
-    this.httpClient.get
+    return this.httpClient.get(url);
   }
 }
