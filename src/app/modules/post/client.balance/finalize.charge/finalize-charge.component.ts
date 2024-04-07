d64533a190ada5b7a2ad708d48465e574482974d
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-08 01:59:40 +0200

    referr to Finalize and pending in client balance compoenet

--- src/app/modules/post/client.balance/finalize.charge/finalize-charge.component.ts
+++ src/app/modules/post/client.balance/finalize.charge/finalize-charge.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-finalize-charge',
+  selector: 'finalize-charge',
   templateUrl: './finalize-charge.component.html',
   styleUrls: ['./finalize-charge.component.scss']
 })
--- src/app/modules/post/client.balance/pending.insurance/pending-insurance.component.ts
+++ src/app/modules/post/client.balance/pending.insurance/pending-insurance.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pending-insurance',
+  selector: 'pending-insurance',
   templateUrl: './pending-insurance.component.html',
   styleUrls: ['./pending-insurance.component.scss']
 })
