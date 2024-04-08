24084098bf50baf91dd7d8f6c17b098371f8aaef
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-08 16:02:29 +0200

    add findAwaiting to clinet balance serice

--- src/app/modules/post/service/client-balance.service.ts
+++ src/app/modules/post/service/client-balance.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { FilterModel } from '../../invoice/area/session.list/filter/filter.model';
+import { IApiParams } from '../../model/interface/api.params';
+import { BasePaginationService } from '../../model/service/base-pagination.service';
+import { PostingFilterModel } from '../bip/filter/posting.filter.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClientBalanceService {
+export class ClientBalanceService extends BasePaginationService  {
+  private baseUrl = environment.baseURL + '/client/balance'
+  constructor(httpClient: HttpClient) { super(httpClient) }
 
-  constructor() { }
+  public findAwaiting(config$: BehaviorSubject<IApiParams>, filterModel: FilterModel): Observable<any> {
+    var url = this.baseUrl + '/find';
+    return this.post(config$, url, JSON.stringify(filterModel))
+  }
+
 }
