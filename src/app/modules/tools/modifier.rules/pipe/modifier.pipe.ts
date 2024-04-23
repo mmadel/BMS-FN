c19db626faee17cd8f1c47a4cefc44284ae71bc4
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-23 22:10:12 +0200

    change modifier pipe from comma to dot

--- src/app/modules/tools/modifier.rules/pipe/modifier.pipe.ts
+++ src/app/modules/tools/modifier.rules/pipe/modifier.pipe.ts
@@ -9,10 +9,10 @@ export class ModifierPipe implements PipeTransform {
     if (!value) return '';
     
     // Remove existing commas and split the string into chunks of two characters
-    const chunks = value.replace(/,/g, '').match(/.{1,2}/g);
+    const chunks = value.replace(/\./g, '').match(/.{1,2}/g);
     
     // Join the chunks with commas and return the result
-    return chunks ? chunks.join(',') : '';
+    return chunks ? chunks.join('.') : '';
   }
 
 }
