d3e72888a1338621f9ffe3cdd562b55210cd0769
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-24 20:03:31 +0200

    when edit cpt apply fee schedule

--- src/app/modules/invoice/area/session.item.edit/session-item-edit.component.ts
+++ src/app/modules/invoice/area/session.item.edit/session-item-edit.component.ts
@@ -1,8 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { debounceTime, filter, finalize, switchMap, tap } from 'rxjs';
 import { CPTCode } from 'src/app/modules/model/clinical/cpt.code';
 import { ServiceCode } from 'src/app/modules/model/clinical/session/service.code';
 import { PatientSessionService } from 'src/app/modules/patient/service/session/patient.session.service';
+import { FeeScheduleLine } from 'src/app/modules/tools/fee.schedule/model/fee.schedule.line';
+import { FeeScheduleService } from 'src/app/modules/tools/fee.schedule/service/fee-schedule.service';
 
 @Component({
   selector: 'session-item-edit',
@@ -11,21 +15,29 @@ import { PatientSessionService } from 'src/app/modules/patient/service/session/p
 })
 export class SessionItemEditComponent implements OnInit {
   @Input() serviceCode: ServiceCode
+  @Input() doctorNPI: string
   @Input() itemType: string
   @Output() changeVisibility = new EventEmitter<string>()
   placeOfCodeKeys = Object.keys;
   modifier: string[] = []
+  feeCtrl = new FormControl();
+  feeScheduleLine: FeeScheduleLine;
+  isLoading = false;
   constructor(private patientSessionService: PatientSessionService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private feeScheduleService: FeeScheduleService) { }
 
   ngOnInit(): void {
-    this.modifier= this.serviceCode.cptCode.modifier.split('.');
+    if (this.itemType === 'cpt') {
+      this.fetchcFeeSchdule();
+      this.modifier = this.serviceCode.cptCode.modifier.split('.');
+    }
   }
   edit() {
     var cptCode: CPTCode = {
       serviceCode: this.itemType === 'cpt' ? this.serviceCode.cptCode.serviceCode : null,
       unit: this.itemType === 'unit' ? this.serviceCode.cptCode.unit : null,
-      charge: this.itemType === 'charge' ? this.serviceCode.cptCode.charge : 0.0,
+      charge: this.serviceCode.cptCode.charge
     }
     this.patientSessionService.updateItems(this.serviceCode.id, cptCode)
       .subscribe(result => {
@@ -34,5 +46,55 @@ export class SessionItemEditComponent implements OnInit {
       }, (error) => {
         this.toastr.error("Error during session udpate")
       })
+  }
+  private fetchcFeeSchdule() {
+    this.feeCtrl.valueChanges
+      .pipe(
+        filter(text => {
+          if (text === undefined)
+            return false;
+          if (text.length > 1) {
+            return true
+          } else {
+            this.feeScheduleLine = {};
+            return false;
+          }
+        }),
+        debounceTime(500),
+        tap((value) => {
+          this.feeScheduleLine = {};
+          this.isLoading = true;
+        }),
+        switchMap((value) => {
+          return this.feeScheduleService.findByCpt(this.doctorNPI, value)
+            .pipe(
+              finalize(() => {
+                this.isLoading = false
+              }),
+            )
+        }
+        )
+      )
+      .subscribe(data => {
+        if (data == undefined) {
+          this.feeScheduleLine = {};
+        } else {
+          this.feeScheduleLine = data;
+          this.calculateCharge(this.feeScheduleLine)
+        }
+      },
+        error => {
+          this.isLoading = false
+        });
+  }
+  private calculateCharge(feeScheduleLine: FeeScheduleLine) {
+    switch (this.feeScheduleLine.rateType) {
+      case 'Per_Unit':
+        this.serviceCode.cptCode.charge = this.serviceCode.cptCode.unit * this.feeScheduleLine.chargeAmount;
+        break;
+      case 'Fixed':
+        this.serviceCode.cptCode.charge = this.feeScheduleLine.chargeAmount;
+        break;
+    }
   }
 }
