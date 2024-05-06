8712659ba5180ad08b79c0a7427f45c494297246
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-06 20:14:12 +0300

    create kc guard

--- src/app/modules/secuirty/security.module.ts
+++ src/app/modules/secuirty/security.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { initializer } from './keycloak-initializer';
+import { KeycloakService } from 'keycloak-angular';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      multi: true,
+      deps: [KeycloakService]
+    }
   ]
 })
 export class SecurityModule { }
