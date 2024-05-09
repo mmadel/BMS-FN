083558c863ba91fbe5938fbaf01e229fcb7d14a3
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-09 12:12:57 +0300

    create user permission interface

--- src/app/modules/admin.tools/model/user.permissions.ts
+++ src/app/modules/admin.tools/model/user.permissions.ts
@@ -1,3 +1,16 @@
-export interface UserPermissions{
-    
+import { AdminPermission } from "./admin/admin.permission";
+import { BillingPermission } from "./billing/billing.permission";
+import { ClientPermission } from "./client/client.permission";
+import { FilingPermission } from "./filing/filing.permission";
+import { PaymentPermission } from "./payment/payment.permission";
+import { ProviderPermission } from "./provider/provider.permission";
+
+export interface UserPermissions {
+    billingPermissions?: BillingPermission
+    providerPermissions?: ProviderPermission
+    clientPermissions?: ClientPermission
+    paymentPermissions?: PaymentPermission;
+    filingPermisiions?: FilingPermission
+    adminPermissions?: AdminPermission
+
 }
