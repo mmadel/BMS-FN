e31afe4158dcb6bc123b447ed56cb3324b5b7bce
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-21 17:10:43 +0300

    bind sub billing roles

--- src/app/modules/admin.tools/components/account/create/role.scope.binder.ts
+++ src/app/modules/admin.tools/components/account/create/role.scope.binder.ts
@@ -13,6 +13,18 @@ export class RoleScopeBinder {
                 case Role.BILLING_ROLE:
                     this.bindBillingRole(roleScope[i].scope)
                     break;
+
+                case Role.INVOICE_BILLING_ROLE:
+                    this.bindBillingInsuranceAreaRole(roleScope[i].scope)
+                    break;
+
+                case Role.FEE_SCHEDULE_BILLING_ROLE:
+                    this.bindBillingFeeRole(roleScope[i].scope)
+                    break;
+
+                case Role.MODIFIER_RULE_BILLING_ROLE:
+                    this.bindBillingModifierRuleRole(roleScope[i].scope)
+                    break;
                 case Role.PROVIDER_ROLE:
                     this.bindProviderRole(roleScope[i].scope)
                     break;
@@ -89,6 +101,37 @@ export class RoleScopeBinder {
                 break;
             case Scope.MODIFYSCOPE:
                 this.roleBinder.adminToolM = true
+                break;
+        }
+    }
+
+    private static bindBillingInsuranceAreaRole(scope: string) {
+        switch (scope) {
+            case Scope.VIEWSCOPE:
+                this.roleBinder.billing_insuranceV = true
+                break;
+            case Scope.MODIFYSCOPE:
+                this.roleBinder.billing_insuranceV = true
+                break;
+        }
+    }
+    private static bindBillingFeeRole(scope: string) {
+        switch (scope) {
+            case Scope.VIEWSCOPE:
+                this.roleBinder.billing_FeeV = true
+                break;
+            case Scope.MODIFYSCOPE:
+                this.roleBinder.billing_FeeM = true
+                break;
+        }
+    }
+    private static bindBillingModifierRuleRole(scope: string) {
+        switch (scope) {
+            case Scope.VIEWSCOPE:
+                this.roleBinder.billing_Modifier_RuleV = true
+                break;
+            case Scope.MODIFYSCOPE:
+                this.roleBinder.billing_Modifier_RuleV = true
                 break;
         }
     }
