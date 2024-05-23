2e7f24751d377949570baaaddcf5886e6719489a
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-23 22:48:00 +0300

    Dates on the left side is a button to open session details like in filing.

--- src/app/modules/cms.filing/cms-filing.module.ts
+++ src/app/modules/cms.filing/cms-filing.module.ts
@@ -1,59 +1,62 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { CmsFilingRoutingModule } from './cms-filing-routing.module';
-import { FindHistoryComponent } from './component/list/find-history.component';
-import { ProgressModule, CalloutModule, AlertModule, BadgeModule, ButtonModule, CardModule, CollapseModule, GridModule, SharedModule, SmartTableModule, TableModule, FormModule, DatePickerModule, DropdownModule, ButtonGroupModule, ListGroupModule, TooltipModule, TabsModule, NavModule, DateRangePickerModule, TimePickerModule, ModalModule, MultiSelectModule, SpinnerModule, SmartPaginationModule } from '@coreui/angular-pro';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { AlertModule, BadgeModule, ButtonGroupModule, ButtonModule, CalloutModule, CardModule, CollapseModule, DatePickerModule, DateRangePickerModule, DropdownModule, FormModule, GridModule, ListGroupModule, ModalModule, MultiSelectModule, NavModule, ProgressModule, SharedModule, SmartPaginationModule, SmartTableModule, SpinnerModule, TableModule, TabsModule, TimePickerModule, TooltipModule } from '@coreui/angular-pro';
 import { IconModule } from '@coreui/icons-angular';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ModelModule } from '../model/model.module';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { SessionHistoryItemComponent } from './component/item/session-history-item.component';
-import { ShowActionsComponent } from './component/actions/show/show-actions.component';
+import { PatientModule } from '../patient';
+import { CmsFilingRoutingModule } from './cms-filing-routing.module';
 import { CorrectClaimActionComponent } from './component/actions/correct.claim/correct-claim-action.component';
+import { ShowActionsComponent } from './component/actions/show/show-actions.component';
+import { SessionHistoryItemComponent } from './component/item/session-history-item.component';
+import { FindHistoryComponent } from './component/list/find-history.component';
 
 @NgModule({
-  declarations: [
-    FindHistoryComponent,
-    SessionHistoryItemComponent,
-    ShowActionsComponent,
-    CorrectClaimActionComponent
-  ],
-  imports: [
-    CommonModule,
-    CmsFilingRoutingModule,
-    ModelModule,
-    AlertModule,
-    BadgeModule,
-    ButtonModule,
-    CardModule,
-    CollapseModule,
-    GridModule,
-    SharedModule,
-    SmartTableModule,
-    TableModule,
-    FormModule,
-    DatePickerModule,
-    DropdownModule,
-    ButtonGroupModule,
-    ListGroupModule,
-    IconModule,
-    TooltipModule,
-    TabsModule,
-    NavModule,
-    DateRangePickerModule,
-    TimePickerModule,
-    AutocompleteLibModule,
-    ModalModule,
-    ModelModule,
-    MultiSelectModule,
-    SpinnerModule,
-    SmartPaginationModule,
-    CalloutModule,
-    MatPaginatorModule,
-    ProgressModule,
-    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
-  ]
+    declarations: [
+        FindHistoryComponent,
+        SessionHistoryItemComponent,
+        ShowActionsComponent,
+        CorrectClaimActionComponent
+    ],
+    imports: [
+        CommonModule,
+        CmsFilingRoutingModule,
+        ModelModule,
+        AlertModule,
+        BadgeModule,
+        ButtonModule,
+        CardModule,
+        CollapseModule,
+        GridModule,
+        SharedModule,
+        SmartTableModule,
+        TableModule,
+        FormModule,
+        DatePickerModule,
+        DropdownModule,
+        ButtonGroupModule,
+        ListGroupModule,
+        IconModule,
+        TooltipModule,
+        TabsModule,
+        NavModule,
+        DateRangePickerModule,
+        TimePickerModule,
+        AutocompleteLibModule,
+        ModalModule,
+        ModelModule,
+        MultiSelectModule,
+        SpinnerModule,
+        SmartPaginationModule,
+        CalloutModule,
+        MatPaginatorModule,
+        ProgressModule,
+        PatientModule,
+        NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
+        
+    ]
 })
 export class CmsFilingModule { }
