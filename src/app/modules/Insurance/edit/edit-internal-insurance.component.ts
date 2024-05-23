98d92194bb163d9e6d071dfbd873ac36f90d1eeb
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-23 14:51:48 +0300

    create skeleton of edit insurance component

---
+++ src/app/modules/Insurance/edit/edit-internal-insurance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-internal-insurance',
+  templateUrl: './edit-internal-insurance.component.html',
+  styleUrls: ['./edit-internal-insurance.component.scss']
+})
+export class EditInternalInsuranceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
