54a6141a219db5fe8f43b0c2d77a8ae3151f576a
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-23 14:11:28 +0300

    call update clinic service

--- src/app/modules/admin.tools/components/clinics/create/create-facility.component.ts
+++ src/app/modules/admin.tools/components/clinics/create/create-facility.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Clinic } from 'src/app/modules/model/admin/clinic';
 import { Role } from 'src/app/modules/secuirty/model/roles';
 import { ClinicService } from '../../../services/clinic.service';
@@ -13,7 +14,7 @@ export class CreateFacilityComponent implements OnInit {
   clinic: Clinic;
   @Output() changeVisibility = new EventEmitter<string>()
   @Input() selectedClinic: Clinic;
-  constructor(private clinicService: ClinicService) { }
+  constructor(private clinicService: ClinicService, private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     if (this.selectedClinic)
@@ -30,13 +31,30 @@ export class CreateFacilityComponent implements OnInit {
     this.clinic = this.selectedClinic;
   }
   add() {
-    this.clinicService.create(this.clinic)
-      .subscribe((result) => {
-        this.changeVisibility.emit('create');
+    if (this.selectedClinic) {
+      this.clinicService.update(this.clinic).subscribe(result => {
+        this.toastrService.success('Clinic Updated');
+        this.changeVisibility.emit('update');
+      }, error => {
+        this.toastrService.error('Error during update clinic');
       })
+    } else {
+      this.clinicService.create(this.clinic)
+        .subscribe((result) => {
+          this.toastrService.success('Clinic Created');
+          this.changeVisibility.emit('create');
+        }, error => {
+          this.toastrService.error('Error during create clinic');
+        })
+    }
+    this.scrollUp()
   }
-  edit() {
-    this.changeVisibility.emit('update');
+  scrollUp() {
+    (function smoothscroll() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.scrollTo(0, 0);
+      }
+    })();
   }
-
 }
