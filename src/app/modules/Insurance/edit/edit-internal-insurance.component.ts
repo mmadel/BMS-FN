350037933810eef5dc61d5896791fcc2563349b3
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-23 15:15:44 +0300

    add address fields in edit insurance component

--- src/app/modules/Insurance/edit/edit-internal-insurance.component.ts
+++ src/app/modules/Insurance/edit/edit-internal-insurance.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { BasicAddress } from '../../model/common/basic.address';
+import { States } from '../../model/lookups/state-data-store';
 
 @Component({
-  selector: 'app-edit-internal-insurance',
+  selector: 'edit-internal-insurance',
   templateUrl: './edit-internal-insurance.component.html',
   styleUrls: ['./edit-internal-insurance.component.scss']
 })
 export class EditInternalInsuranceComponent implements OnInit {
-
+  states: string[] = States;
+  address:BasicAddress={state:null}
   constructor() { }
 
   ngOnInit(): void {
