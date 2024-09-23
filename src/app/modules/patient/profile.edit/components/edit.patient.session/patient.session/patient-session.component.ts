65858566718b38c4b26d823f7a7982c817193b32
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-09-23 23:56:41 +0300

    create scheduleing card in patient session

--- src/app/modules/patient/profile.edit/components/edit.patient.session/patient.session/patient-session.component.ts
+++ src/app/modules/patient/profile.edit/components/edit.patient.session/patient.session/patient-session.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Provider } from 'src/app/modules/model/clinical/provider/provider';
+import { EmitPatientSessionService } from 'src/app/modules/patient/service/session/shared/emit-patient-session.service';
+import { SessionScheduling } from 'src/app/modules/patient/session/model/session.scheduling';
+import { ProviderService } from 'src/app/modules/providers/service/provider.service';
+interface ProviderInfo {
+  name: string,
+  model: Provider
+}
 @Component({
   selector: 'patient-session',
   templateUrl: './patient-session.component.html',
   styleUrls: ['./patient-session.component.scss']
 })
 export class PatientSessionComponent implements OnInit {
-
-  constructor() { }
+  notValidForm: boolean = false;
+  providers: ProviderInfo[];
+  sessionScheduling: SessionScheduling={};
+  constructor(private providerService: ProviderService,private emitPatientSessionService: EmitPatientSessionService) { }
 
   ngOnInit(): void {
+    this.findProviders();
+  }
+  private findProviders() {
+    this.providerService.findAllWithoutPagination()
+      .subscribe((result: any) => {
+        this.providers = new Array();
+        for (var i = 0; i < result.length; i++) {
+          this.providers.push({
+            name: result[i].lastName + ',' + result[i].firstName,
+            model: result[i]
+          })
+        }
+      })
+  }
+  pickProvider(event: any) {
+    this.emitPatientSessionService.selectedProvider$.next(event)
   }
 
 }
