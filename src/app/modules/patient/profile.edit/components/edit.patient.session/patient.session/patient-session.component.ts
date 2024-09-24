0f5df3500c13b85ba75007f010a19790efffab9e
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-09-24 09:37:12 +0300

    add icd10 in billing card

--- src/app/modules/patient/profile.edit/components/edit.patient.session/patient.session/patient-session.component.ts
+++ src/app/modules/patient/profile.edit/components/edit.patient.session/patient.session/patient-session.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ClinicService } from 'src/app/modules/admin.tools/services/clinic.service';
+import { Clinic } from 'src/app/modules/model/admin/clinic';
+import { CaseDiagnosis } from 'src/app/modules/model/clinical/case.diagnosis';
+import { PatientCase } from 'src/app/modules/model/clinical/patient.case';
 import { Provider } from 'src/app/modules/model/clinical/provider/provider';
+import { PlaceOfCode } from 'src/app/modules/model/enum/place.code';
+import { BillingCode } from 'src/app/modules/patient/profile/filling/sessions/model/billing.code';
+import { CaseDiagnosisService } from 'src/app/modules/patient/service/case.diagnosis/case-diagnosis.service';
 import { EmitPatientSessionService } from 'src/app/modules/patient/service/session/shared/emit-patient-session.service';
 import { SessionScheduling } from 'src/app/modules/patient/session/model/session.scheduling';
 import { ProviderService } from 'src/app/modules/providers/service/provider.service';
@@ -15,11 +22,27 @@ interface ProviderInfo {
 export class PatientSessionComponent implements OnInit {
   notValidForm: boolean = false;
   providers: ProviderInfo[];
-  sessionScheduling: SessionScheduling={};
-  constructor(private providerService: ProviderService,private emitPatientSessionService: EmitPatientSessionService) { }
+  sessionScheduling: SessionScheduling = {};
+  billingCode: BillingCode = { placeOfCode: null, facility: null }
+  placeOfCodeKeys = Object.keys;
+  placeOfCodes = PlaceOfCode;
+  compareFn = this._compareFn.bind(this);
+  clinics: Clinic[];
+  @Input() patientCases: PatientCase[];
+  selectedCase: any = null;
+  diagnosisValue: string
+  isLoading = false;
+  diagnosisCode?: CaseDiagnosis = {};
+  filteredDiagnosis: any;
+  diagnosises: CaseDiagnosis[]=[];
+  constructor(private providerService: ProviderService
+    , private emitPatientSessionService: EmitPatientSessionService
+    , private clinicService: ClinicService
+    ,private caseDiagnosisService: CaseDiagnosisService) { }
 
   ngOnInit(): void {
     this.findProviders();
+    this.findClinics()
   }
   private findProviders() {
     this.providerService.findAllWithoutPagination()
@@ -36,5 +59,38 @@ export class PatientSessionComponent implements OnInit {
   pickProvider(event: any) {
     this.emitPatientSessionService.selectedProvider$.next(event)
   }
-
+  _compareFn(a: any, b: any) {
+    return a?.id === b?.id;
+  }
+  findClinics() {
+    this.clinicService.findAllWithoutPagination()
+      .subscribe((result: any) => {
+        this.clinics = result
+      })
+  }
+  change() {
+    this.billingCode.caseTitle = this.selectedCase.caseTitle;
+    this.diagnosises.push(...this.selectedCase.caseDiagnosis)
+  }
+  search() {
+    this.caseDiagnosisService.find(this.diagnosisValue).subscribe(data => {
+      console.log(JSON.stringify(data))
+      if (data !== undefined) {
+        var diagnosisResponse: any = data;
+        this.filteredDiagnosis = diagnosisResponse.listOfCodeName;
+      }
+    })
+  }
+  addICD10diagnosis(event: any) {
+    var diagnosis: string = event.target.value
+    var diagnosisArr = diagnosis.split(',')
+    var code: string = diagnosisArr[0]
+    var desrciption: string = diagnosisArr.slice(1).toString();
+    this.diagnosisCode.diagnosisCode = code
+    this.diagnosisCode.diagnosisDescription = desrciption;
+  }
+  pushDaignosis(){
+    this.diagnosises.push(this.diagnosisCode);
+    this.diagnosisCode={}
+  }
 }
