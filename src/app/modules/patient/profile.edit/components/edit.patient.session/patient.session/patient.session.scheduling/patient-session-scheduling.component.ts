6d44b2bcba49467293b5460fd50cbc0bf21a7b13
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-09-25 18:17:25 +0300

    handle edit specific session scheduling part

--- src/app/modules/patient/profile.edit/components/edit.patient.session/patient.session/patient.session.scheduling/patient-session-scheduling.component.ts
+++ src/app/modules/patient/profile.edit/components/edit.patient.session/patient.session/patient.session.scheduling/patient-session-scheduling.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Provider } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { Provider } from 'src/app/modules/model/clinical/provider/provider';
+import { PatientSession } from 'src/app/modules/model/clinical/session/patient.session';
 import { SessionScheduling } from 'src/app/modules/patient/session/model/session.scheduling';
 import { ProviderService } from 'src/app/modules/providers/service/provider.service';
 interface ProviderInfo {
@@ -11,6 +14,7 @@ interface ProviderInfo {
   styleUrls: ['./patient-session-scheduling.component.scss']
 })
 export class PatientSessionSchedulingComponent implements OnInit {
+  @Input() selectedSession: PatientSession
   notValidForm: boolean = false;
   providers: ProviderInfo[];
   sessionScheduling: SessionScheduling = {};
@@ -18,9 +22,31 @@ export class PatientSessionSchedulingComponent implements OnInit {
 
   ngOnInit(): void {
     this.findProviders();
+    this.fillModel();
   }
+
   pickProvider(event: any) {
     this.providerService.selectedProvider$.next(event.model)
+  }
+  private fillModel() {
+    if (this.selectedSession !== undefined) {
+      var provider: ProviderInfo = {
+        name: this.selectedSession.doctorInfo.doctorLastName + ',' + this.selectedSession.doctorInfo.doctorFirstName,
+        model: {
+          firstName: this.selectedSession.doctorInfo.doctorFirstName,
+          lastName: this.selectedSession.doctorInfo.doctorLastName,
+          npi: this.selectedSession.doctorInfo.doctorNPI
+        }
+      }
+      this.sessionScheduling = {
+        provider: provider,
+        serviceDate: moment.unix(this.selectedSession.serviceDate / 1000).toDate(),
+        startTime: moment.unix(this.selectedSession.serviceStartTime / 1000).toDate(),
+        endTime: moment.unix(this.selectedSession.serviceEndTime / 1000).toDate()
+
+      }
+      this.providerService.selectedProvider$.next(provider.model)
+    }
   }
   private findProviders() {
     this.providerService.findAllWithoutPagination()
